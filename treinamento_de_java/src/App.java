f62b5a6f53b9232a82205ffe8aed178fc6fac764
msilva0706 <marcio070690@gmail.com>
2025-03-11 00:47:12 -0300

    alteraçao no codigo

--- treinamento_de_java/src/App.java
+++ treinamento_de_java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("vamos aprender");
+        System.out.println("vamos aprender meu povo");
     }
 }
